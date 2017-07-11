0947448571499196ff4e92f191929e98f703c6be
Dzierzanowski <tomasz.dzierzanowski@hpe.com>
2017-07-11 23:51:45 +0200

    lala

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World of azure!");
+    response.end("Hello World of azure 2!");
 
 });
 
